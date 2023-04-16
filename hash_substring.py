d4b01b1924ade514826c7625c5adf388f19b4570
221RDB286 <125820885+eduards4grupa@users.noreply.github.com>
2023-04-17 01:07:09 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -3,8 +3,8 @@
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    input_type = input().rstrip()
-    if input_type == "f":
+    #input_type = input().rstrip()
+    if "F" in input():
         with open("tests/06") as f:
             patt = f.readline().rstrip()
             text = f.readline().rstrip()
