8e6306884aa6dc31494c0fabe6e223d8d9d5ac04
221RDB286 <125820885+eduards4grupa@users.noreply.github.com>
2023-04-17 00:21:01 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -3,17 +3,18 @@
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
+    input_type = input().rstrip()
     
     # after input type choice
     # read two lines 
     # first line is pattern 
     # second line is text in which to look for pattern 
-    
+    patt = input().rstrip()
+    text = input().rstrip()
     # return both lines in one return
-    
+   
     # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    return patt, text
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
@@ -21,9 +22,34 @@ def print_occurrences(output):
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
-
+    n = len(text)
+    m=len(patt)
+    p=31
+    m_mod = 10**9+9
+    p_pow=[1]
+    for i in range(1,m):
+        p_pow.append((p_pow[-1]*p)%m_mod)
+        
+    h=[0]*(n+1)
+    for i in range(1,n+1):
+        h[i] = (h[h-1] + (ord(tex[i-1]) - ord('a')+1)*p_pow[i-1])% m_mod
+        
+    patt_hash=0
+    for i in range(m):
+        patt_hash=(patt_hash+(ord(patt[i])-ord('a')+1)*p_pow[i] % m_mod
+   
+    occurrences=[]
+    for i in range(n-m+1):
+        current_hash = (h[i+m)-h[i]+m_mod)% m_mod
+        if current_hash == patt_hash:
+            if text[i:i+m] == patt
+                occurrences.append(i)
+                          
+        
+                   
+                   
     # and return an iterable variable
-    return [0]
+    return occurrences
 
 
 # this part launches the functions
